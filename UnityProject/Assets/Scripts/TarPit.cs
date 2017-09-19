4b96e7f99eca3607551428168e9bd8d2ecdfc40b
Goodii <pgoodison98@gmail.com>
2017-09-19 16:57:32 +1000

    Added DefenseTrap with cost Added TarPit (minuses x from player's speed)

---
+++ UnityProject/Assets/Scripts/TarPit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TarPit : DefenseTrap {
+
+    public int speedReduction;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Collider collision = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {		
+	}
+
+    void OnCollisionEnter(Collision a_col)
+    {
+        a_col.collider.GetComponent<PlayerController>().speed -= speedReduction;
+        Debug.Log("Enter: " + a_col.collider.name);
+    }
+
+    void OnCollisionExit(Collision a_col)
+    {
+        a_col.collider.GetComponent<PlayerController>().speed += speedReduction;
+        Debug.Log("Exit: " + a_col.collider.name);
+    }
+}

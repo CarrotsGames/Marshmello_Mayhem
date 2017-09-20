4c2c34ffb4688b92bbb26825f394c64279fc8724
Goodii <pgoodison98@gmail.com>
2017-09-20 10:41:36 +1000

    Added ResourceController (for players, increments over time) Added ResourcePickUp (for items, increases player's resource on pick up)

---
+++ UnityProject/Assets/Scripts/ResourceController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//for each player
+//modifiable values:
+//  maxResource, currentResource, resourceIncrease, timeBetween
+
+public class ResourceController : MonoBehaviour
+{
+
+    public int maxResource;
+    public int currentResource;
+    public int resourceIncrease;
+    public float timeBetween;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentResource = 0;
+        TimeBetween();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentResource > maxResource)
+        {
+            currentResource = maxResource;
+        }
+    }
+
+    void TimeBetween()
+    {
+        Invoke("IncrementResources", timeBetween);
+    }
+
+    void IncrementResources()
+    {
+        Invoke("TimeBetween", timeBetween);
+        currentResource += resourceIncrease;
+    }
+}
---
+++ UnityProject/Assets/Scripts/ResourcePickUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//for each pick up item
+//modifiable values:
+//  pickUpIncrease (the amount of resources gained)
+
+public class ResourcePickUp : MonoBehaviour {
+
+    public int resourceGain;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider a_col)
+    {
+        a_col.GetComponent<ResourceController>().currentResource += resourceGain;
+        Destroy(gameObject);
+    }
+}

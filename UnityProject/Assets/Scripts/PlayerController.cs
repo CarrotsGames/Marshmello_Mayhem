98e8bc9815c62b9add51b36bf95f9480d066874f
Brendon Catania <nimbussparkwave@gmail.com>
2017-09-20 12:22:53 +1000

    Added in PlayerController.cs, PlayerHealth.cs and Projectile.cs. Additionally added in a UI for Health however will likely not appear when committed.

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XboxCtrlrInput;
 
 public class PlayerController : MonoBehaviour {
 
+    public Rigidbody rigidBody;
+    public XboxController controller;
+
     public float speed;
+    public float maxSpeed = 5;
+    public Vector3 previousRotation = Vector3.forward;
+    private float attackDelay;
+    public float attackDamage;
+    public float timeBetweenAttack = 0.02f;
+    public bool playerMovement = true;
 
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotatePlayer();
+    }
+
+        private void RotatePlayer()
+    {
+        float rotateAxisX = XCI.GetAxis(XboxAxis.RightStickX, controller);
+        float rotateAxisZ = XCI.GetAxis(XboxAxis.RightStickY, controller);
+        Vector3 directionVector = new Vector3(rotateAxisX, 0, rotateAxisZ);
+
+        if (directionVector.magnitude < 0.1f)
+        {
+            directionVector = previousRotation;
+        }
+
+        directionVector = directionVector.normalized;
+        previousRotation = directionVector;
+        transform.rotation = Quaternion.LookRotation(directionVector);
+    }
+
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
@@ -21,5 +52,21 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal * (speed * Time.deltaTime), 0, moveVertical * (speed * Time.deltaTime));
 
         transform.Translate(movement);
-	}
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float axisX = XCI.GetAxis(XboxAxis.LeftStickX, controller);
+        float axisZ = XCI.GetAxis(XboxAxis.LeftStickY, controller);
+
+        Vector3 movement = new Vector3(axisX, 0, axisZ);
+
+        rigidBody.AddForce(movement * speed);
+
+        if (rigidBody.velocity.magnitude > maxSpeed)
+        {
+            rigidBody.velocity = rigidBody.velocity.normalized * maxSpeed;
+        }
+    }
 }
---
+++ UnityProject/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 10;
+    public int currentHealth;
+    public Slider HealthSlider;
+    bool isDead;
+    bool damaged;
+
+    // Use this for initialization
+    void Start ()
+    {
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(damaged)
+        {
+
+        }
+
+        damaged = false;
+	}
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+        if (!isDead)
+        {
+            currentHealth -= amount;
+            HealthSlider.value = currentHealth - attackDamage;
+        }
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        playerMovement.enabled = false;
+
+    }
+}

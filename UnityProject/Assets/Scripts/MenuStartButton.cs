8bff1627c61a95c1368d148ed55eaaa30db9ef50
Goodii <pgoodison98@gmail.com>
2017-11-13 09:10:54 +1100

    Attach to the start button in menu. Loads level with the same name as the levelName public string on script

---
+++ UnityProject/Assets/Scripts/MenuStartButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuStartButton : MonoBehaviour {
+
+    public string levelName = "Greybox";
+    Button startButton;
+
+	// Use this for initialization
+	void Start () {
+        startButton = GetComponent<Button>();
+        startButton.onClick.AddListener(ChangeScene);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void ChangeScene()
+    {
+        Application.LoadLevel(levelName);
+    }
+}

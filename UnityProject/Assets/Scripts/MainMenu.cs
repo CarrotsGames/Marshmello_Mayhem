9dd96e76f70c750a18134bcaefe26e0f1e03be1f
Goodii <pgoodison98@gmail.com>
2017-11-13 09:25:15 +1100

    Attach MainMenu script to Menu in the canvas, then attach both Start and Quit buttons to their respective slots in the script

--- UnityProject/Assets/Scripts/MainMenu.cs
+++ UnityProject/Assets/Scripts/MainMenu.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MenuStartButton : MonoBehaviour {
+public class MainMenu : MonoBehaviour {
 
+    public Button startButton;
+    public Button quitButton;
     public string levelName = "Greybox";
-    Button startButton;
 
 	// Use this for initialization
 	void Start () {
-        startButton = GetComponent<Button>();
         startButton.onClick.AddListener(ChangeScene);
+
+        quitButton.onClick.AddListener(Quit);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+		
 	}
 
     void ChangeScene()
     {
         Application.LoadLevel(levelName);
+    }
+
+    void Quit()
+    {
+        Application.Quit();
     }
 }

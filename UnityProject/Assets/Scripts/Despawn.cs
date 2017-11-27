7ed64062cf0eb43af8f3a6bceea84218b0b38c6a
Goodii <pgoodison98@gmail.com>
2017-11-28 09:18:44 +1100

    Destroys object after timer (destruction is immediate)

---
+++ UnityProject/Assets/Scripts/Despawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Despawn : MonoBehaviour {
+
+    public float timeBeforeDespawn = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeBeforeDespawn -= Time.deltaTime;
+
+        if (timeBeforeDespawn <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
